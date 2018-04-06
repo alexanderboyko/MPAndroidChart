585abd8ff78b480a21f62d9d09f2b5d8989744e6
Sasha <alexboyko426@gmail.com>
2018-04-06 03:00:06 +0200

    content rect top padding

--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/BarChartRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/BarChartRenderer.java
@@ -4,12 +4,8 @@ package com.github.mikephil.charting.renderer;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 
 import com.github.mikephil.charting.animation.ChartAnimator;
 import com.github.mikephil.charting.buffer.BarBuffer;
@@ -182,6 +178,7 @@ public class BarChartRenderer extends BarLineScatterCandleBubbleRenderer {
     }
 
     private void drawRoundedRect(Canvas canvas, Paint paint, RectF rect) {
+        rect.top -=8f;
         canvas.drawRoundRect(rect, 10, 10, paint);
         canvas.drawRect(rect.left, rect.top + 10, rect.right, rect.bottom, paint);
     }
